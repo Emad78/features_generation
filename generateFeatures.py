21b98e6feb9c41b7039e53d83f8479e3b9492aba
Emad78 <49594916+Emad78@users.noreply.github.com>
2022-09-06 18:33:22 +0430

    Fixed

--- generateFeatures.py
+++ generateFeatures.py
@@ -1,8 +1,10 @@
 import pandas as pd
+import os
 from ta.momentum import RSIIndicator, StochRSIIndicator, WilliamsRIndicator, AwesomeOscillatorIndicator
 from ta.volume import MFIIndicator, OnBalanceVolumeIndicator
 from ta.trend import SMAIndicator, EMAIndicator, MACD, ADXIndicator, IchimokuIndicator, CCIIndicator, AroonIndicator
 from ta.volatility import BollingerBands
+import numpy as np
 
 CLOSE_COLUMN = 'finalPrice'
 HIGH_COLUMN = 'highestPrice'
@@ -14,46 +16,57 @@ LONG_TERM = 28
 NAME_COLUMN = 'Lval18AFC'
 
 def find_SR(closePrice, period):
-  sr = []
-  for i in range(period, len(closePrice)-period):
-    if closePrice[i] >= max(closePrice[i-period:i+period]) or closePrice[i] <= min(closePrice[i-period:i+period]):
-      sr.append(closePrice[i])
-  return sr
+  try:
+    sr = []
+    for i in range(period, len(closePrice)-period):
+      if closePrice[i] >= max(closePrice[i-period:i+period]) or closePrice[i] <= min(closePrice[i-period:i+period]):
+        sr.append(closePrice[i])
+    return sr
+  except:
+    pass
 
 def add_sr_features(data:pd.DataFrame):
-  period = MID_TERM
-  sr = find_SR(data[CLOSE_COLUMN], period)
-  print(len(sr))
-  highMinDistanceSR = []
-  lowMinDistanceSR = []
-  closeMinDistanceSR = []
-  for i in range(len(data)):
-    highMinDistanceSR.append(min(list(map(lambda x: abs(x - data.loc[i, HIGH_COLUMN]),sr))))
-    lowMinDistanceSR.append(min(list(map(lambda x: abs(x - data.loc[i, LOW_COLUMN]),sr))))
-    closeMinDistanceSR.append(min(list(map(lambda x: abs(x - data.loc[i, CLOSE_COLUMN]),sr))))
-  data['high_min_distance_sr'] = highMinDistanceSR 
-  data['low_min_distance_sr'] = lowMinDistanceSR 
-  data['close_min_distance_sr'] = closeMinDistanceSR 
-  
+  try:
+    period = MID_TERM
+    sr = find_SR(data[CLOSE_COLUMN], period)
+    print(len(sr))
+    highMinDistanceSR = []
+    lowMinDistanceSR = []
+    closeMinDistanceSR = []
+    for i in range(len(data)):
+      highMinDistanceSR.append(min(list(map(lambda x: abs(x - data.loc[i, HIGH_COLUMN]),sr))))
+      lowMinDistanceSR.append(min(list(map(lambda x: abs(x - data.loc[i, LOW_COLUMN]),sr))))
+      closeMinDistanceSR.append(min(list(map(lambda x: abs(x - data.loc[i, CLOSE_COLUMN]),sr))))
+    data['high_min_distance_sr'] = highMinDistanceSR 
+    data['low_min_distance_sr'] = lowMinDistanceSR 
+    data['close_min_distance_sr'] = closeMinDistanceSR 
+  except:
+    pass  
 # cross value
 def cross_value_from_bottom(column, value):
   crossed = [0] * len(column)
-  for i in range(1, len(column)):
-    if(column[i] >= value) and (column[i-1] < value):
-      crossed[i] = 1
-
+  try:
+    for i in range(1, len(column)):
+      if(column[i] >= value) and (column[i-1] < value):
+        crossed[i] = 1
+  except:
+    pass
   return crossed
 
 def cross_value_from_above(column, value):
   crossed = [0] * len(column)
-  for i in range(1, len(column)):
-    if (column[i] <= value) and (column[i-1] > value):
-      crossed[i] = 1
-
+  try:
+    for i in range(1, len(column)):
+      if (column[i] <= value) and (column[i-1] > value):
+        crossed[i] = 1
+  except:
+    pass
   return crossed
  
 # sum in period
 def sum_of_value_in_a_period(column, period):
+  if len(column) < period:
+    return [0]*len(column)
   sum_in_period = [0] * len(column)
   for i in range(period):
     sum_in_period[i] = 0
@@ -128,6 +141,8 @@ def up_down_line(column, line):
 
 
 def trend_up(column, period):
+  if period >= len(column):
+    return [0]*len(column)
   trend = [0]*period + [1] * (len(column)-period)
   for i in range(period, len(column)):
     if min(column[i-period:i+1]) < column[i-period]:
@@ -135,6 +150,8 @@ def trend_up(column, period):
   return trend
 
 def trend_down(column, period):
+  if period >= len(column):
+    return [0]*len(column)
   trend = [0]*period + [1] * (len(column)-period)
   for i in range(period, len(column)):
     if max(column[i-period:i+1]) > column[i-period]:
@@ -185,6 +202,7 @@ def is_on_range(line, lowerBound, upperBound):
     return onRange
 
 def add_rsi_features(data:pd.DataFrame):
+  try:
     rsi = RSIIndicator(data[CLOSE_COLUMN], fillna=True).rsi()
     data['rsi'] = rsi
     data['rsi_cross_70_from_bottom'] = cross_value_from_bottom(data['rsi'], 70)
@@ -195,9 +213,11 @@ def add_rsi_features(data:pd.DataFrame):
     data['rsi_cross_50_from_above'] = cross_value_from_above(data['rsi'], 50)
     data['rsi_trendUp'] = trend_up(data['rsi'], 14)
     data['rsi_trendDown'] = trend_down(data['rsi'], 14)
-    #count candle
+  except:
+    pass
 
 def add_mfi_features(data:pd.DataFrame):
+  try:
     mfi = MFIIndicator(data[HIGH_COLUMN], data[LOW_COLUMN], data[CLOSE_COLUMN], data[VOLUME_COLUMN], fillna=True).money_flow_index()
     data['mfi'] = mfi
     data['mfi_cross_80_from_bottom'] = cross_value_from_bottom(data['mfi'], 80)
@@ -207,8 +227,11 @@ def add_mfi_features(data:pd.DataFrame):
     data['mfi_sum'] = sum_of_value_in_a_period(data['mfi'], 14)
     data['mfi_trendUp'] = trend_up(data['mfi'], 14)
     data['mfi_trendDown'] = trend_down(data['mfi'], 14)
+  except:
+    pass
 
 def add_ichi_features(data:pd.DataFrame):
+  try:
     ichi = IchimokuIndicator(data[HIGH_COLUMN], data[LOW_COLUMN], fillna=True)
     data['ichi_spanA'] = ichi.ichimoku_a()
     data['ichi_spanB'] = ichi.ichimoku_b()
@@ -223,8 +246,11 @@ def add_ichi_features(data:pd.DataFrame):
 
     data['ichi_price_on_greenCloud'] = is_on_range(data[CLOSE_COLUMN], data['ichi_spanB'], data['ichi_spanA'])
     data['ichi_price_on_redCloud'] = is_on_range(data[CLOSE_COLUMN], data['ichi_spanA'], data['ichi_spanB'])
+  except:
+    pass
 
 def add_bb_features(data:pd.DataFrame):
+  try:
     bb = BollingerBands(data[CLOSE_COLUMN], fillna=True)
     data['bb_lowerBound'] = bb.bollinger_lband()
     data['bb_upperBound'] = bb.bollinger_hband()
@@ -238,10 +264,13 @@ def add_bb_features(data:pd.DataFrame):
     
     data['bb_bound_expanding'] = bound_expanding(data['bb_upperBound'], data['bb_lowerBound'])
     data['bb_bound_tightening'] = bound_tightening(data['bb_upperBound'], data['bb_lowerBound'])
+  except:
+    pass
 
 
 
 def add_ema_features(data:pd.DataFrame):
+  try:
     ema5 = EMAIndicator(data[CLOSE_COLUMN], 5, True).ema_indicator()
     ema10 = EMAIndicator(data[CLOSE_COLUMN], 10, True).ema_indicator()
     ema20 = EMAIndicator(data[CLOSE_COLUMN], 20, True).ema_indicator()
@@ -275,8 +304,11 @@ def add_ema_features(data:pd.DataFrame):
     data['ema20_cross_ema50_from_above'] = cross_line_from_bottom(data['ema50'], data['ema20'])
     data['ema20_cross_ema100_from_bottom'] = cross_line_from_bottom(data['ema20'], data['ema100'])
     data['ema20_cross_ema100_from_above'] = cross_line_from_bottom(data['ema100'], data['ema20'])
-    
+  except:
+    pass
+
 def add_sma_features(data:pd.DataFrame):
+  try:
     sma5 = SMAIndicator(data[CLOSE_COLUMN], 5, True).sma_indicator()
     sma10 = SMAIndicator(data[CLOSE_COLUMN], 10, True).sma_indicator()
     sma20 = SMAIndicator(data[CLOSE_COLUMN], 20, True).sma_indicator()
@@ -317,8 +349,11 @@ def add_sma_features(data:pd.DataFrame):
     data['sma200_cross_sma30_from_above'] = cross_line_from_bottom(data['sma30'], data['sma200'])
     data['sma200_cross_sma100_from_bottom'] = cross_line_from_bottom(data['sma200'], data['sma100'])
     data['sma200_cross_sma100_from_above'] = cross_line_from_bottom(data['sma100'], data['sma200'])
+  except:
+    pass
 
 def add_cci_features(data:pd.DataFrame):
+  try:
     cci = CCIIndicator(data[HIGH_COLUMN], data[LOW_COLUMN], data[CLOSE_COLUMN], fillna=True).cci()
     data['cci'] = cci
     data['cci_cross_200_from_above'] = cross_value_from_above(data['cci'], 200)
@@ -334,8 +369,11 @@ def add_cci_features(data:pd.DataFrame):
     data['cci_cross_-200_from_bottom'] = cross_value_from_bottom(data['cci'], -200)
     
     data['cci_sum'] = sum_of_value_in_a_period(data['cci'], 14)
+  except:
+    pass
 
 def add_macd_features(data:pd.DataFrame):
+  try:
     macd = MACD(data[CLOSE_COLUMN], fillna=True) 
     line = macd.macd()
     signal = macd.macd_signal()
@@ -356,9 +394,12 @@ def add_macd_features(data:pd.DataFrame):
     
     data['macd_macd_cross_signal_bullish'] = cross_line_bullish(data['macd_macd'], data['macd_signal'])
     data['macd_macd_cross_signal_bearish'] = cross_line_bearish(data['macd_macd'], data['macd_signal'])
+  except:
+    pass
     
  
 def add_stochrsi_features(data:pd.DataFrame):
+  try:
     stochrsi = StochRSIIndicator(data[CLOSE_COLUMN], fillna=True)
     data['stochrsi_stochrsi'] = stochrsi.stochrsi()
     data['stochrsi_d'] = stochrsi.stochrsi_d()
@@ -385,8 +426,11 @@ def add_stochrsi_features(data:pd.DataFrame):
     
     data['stochrsi_k_cross_d_bullish'] = cross_line_bullish(data['stochrsi_k'], data['stochrsi_d'])
     data['stochrsi_k_cross_d_bearish'] = cross_line_bearish(data['stochrsi_k'], data['stochrsi_d'])
+  except:
+    pass
 
 def add_trend_features(data:pd.DataFrame):
+  try:
     close = data[CLOSE_COLUMN]
 
     data['trendUp_5'] = trend_up(close, SHORT_TERM)
@@ -409,8 +453,11 @@ def add_trend_features(data:pd.DataFrame):
     data['trendNeutral_50'] = trend_neutral(data['trendUp_50'], data['trendDown_50'])
     data['trendNeutral_100'] = trend_neutral(data['trendUp_100'], data['trendDown_100'])
     data['trendNeutral_200'] = trend_neutral(data['trendUp_200'], data['trendDown_200'])
+  except:
+    pass
 
 def add_wr_feature(data:pd.DataFrame):
+  try:
     wr = WilliamsRIndicator(data[HIGH_COLUMN], data[LOW_COLUMN], data[CLOSE_COLUMN], fillna=True).williams_r()
     data['wr'] = wr
 
@@ -424,8 +471,11 @@ def add_wr_feature(data:pd.DataFrame):
     
     data['wr_cross_-80_from_above'] = cross_value_from_above(wr, -80)
     data['wr_cross_-80_from_bottom'] = cross_value_from_bottom(wr, -80)
+  except:
+    pass
 
 def add_aroon_features(data:pd.DataFrame):
+  try:
     aroon = AroonIndicator(data[CLOSE_COLUMN], fillna=True)
     data['aroon_up'] = aroon.aroon_up()
     data['aroon_down'] = aroon.aroon_down()
@@ -436,8 +486,11 @@ def add_aroon_features(data:pd.DataFrame):
     
     data['aroon_up_above_down'] = up_down_line(data['aroon_up'], data['aroon_down'])
     data['aroon_down_above_up'] = up_down_line(data['aroon_down'], data['aroon_up'])
+  except:
+    pass
 
 def add_adx_feature(data:pd.DataFrame):
+  try:
     adx = ADXIndicator(data[HIGH_COLUMN], data[LOW_COLUMN], data[CLOSE_COLUMN], fillna=True) 
     data['adx_adx'] = adx.adx()
     data['adx_pos'] = adx.adx_pos()
@@ -448,11 +501,13 @@ def add_adx_feature(data:pd.DataFrame):
     data['adx_pos_cross_neg_from_bottom'] = cross_line_from_bottom(data['adx_pos'], data['adx_neg'])
     data['adx_pos_cross_neg_from_above'] = cross_line_from_bottom(data['adx_neg'], data['adx_pos'])
     
-    data['adx_pos_above_neg'] = up_down_line(data['adx_pos'], data['adx_neg'])  
-    
+    data['adx_pos_above_neg'] = up_down_line(data['adx_pos'], data['adx_neg'])      
+  except:
+    pass
 
 
 def add_person_company_features(data:pd.DataFrame):
+  try:
     data['per_capita_person_buyer'] = data['valueOfPersonBuyer']/data['numOfPersonBuyer']
     data['per_capita_person_seller'] = data['valueOfPersonSeller']/data['numOfPersonSeller']
 
@@ -501,7 +556,9 @@ def add_person_company_features(data:pd.DataFrame):
     data['value_company_buyer_up_than_half_value'] = up_down_line(data['valueOfCompanyBuyer'], data['tradeValue']/2)
     data['volume_company_buyer_up_than_half_volume'] = up_down_line(data['volOfCompanyBuyer'], data['tradeVol']/2)
     data['num_company_buyer_up_than_half_num'] = up_down_line(data['numOfCompanyBuyer'], data['tradeNum']/2)
-        
+  except:
+    pass
+    
 
 def add_indicator_features(data:pd.DataFrame):
     add_rsi_features(data)
@@ -535,3 +592,4 @@ def add_features(data:pd.DataFrame):
         else:
             finalData = pd.concat([finalData, symbolData])
     return finalData 
+
